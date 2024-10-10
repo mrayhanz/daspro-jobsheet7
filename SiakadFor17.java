943777d6874676fcafec55128a8911d5330438fd
unknown <rayymekdi@gmail.com>
2024-10-10 07:51:33 +0700

    pertanyaan1

--- SiakadFor17.java
+++ SiakadFor17.java
@@ -5,6 +5,10 @@ public class SiakadFor17 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        // modifikasi
+        int jumlahLulus = 0; 
+        int jumlahTidaklulus = 0;
+        double batasKelulusan = 60.0;
 
         for(int i  = 1; i <= 10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -15,8 +19,17 @@ public class SiakadFor17 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // modifikasi
+            if (nilai >= batasKelulusan) {
+                jumlahLulus++;
+            } else {
+                jumlahTidaklulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus : " +jumlahLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + jumlahTidaklulus);
     }
 }
